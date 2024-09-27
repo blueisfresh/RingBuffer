f7ce4ef342ba0579f54c818f41dacf7294948020
blueisfresh <elias.guerematchi.vrabl@gmail.com>
2024-09-27 10:49:34 +0200

    Initial commit with folder and code

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿namespace RingBuffer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO: Null Werte im Int
+            RingBuffer<string> ringbufferString = new RingBuffer<string>(5);
+
+            Console.WriteLine("Strings\n");
+
+            ringbufferString.Add("elias1");
+            ringbufferString.Add("elias2");
+            ringbufferString.Add("elias3");
+            ringbufferString.Add("elias4");
+            ringbufferString.Add("elias5");
+            ringbufferString.Add("elias6");
+            ringbufferString.Add("elias7");
+            ringbufferString.Add("elias8");
+            ringbufferString.Add("elias9");
+            // ringbuffer.Add("elias10");
+
+            Console.WriteLine("All The Values got added\n");
+
+            Console.WriteLine(ringbufferString.ToString());
+
+            Console.WriteLine($"\nThe used Size: {ringbufferString.UsedSize}\n");
+
+            ringbufferString.Clear();
+
+            Console.WriteLine("The ringbuffer got cleared \n");
+
+            Console.WriteLine(ringbufferString.ToString());
+
+            RingBuffer<int> ringbufferInt = new RingBuffer<int>(5);
+
+            Console.WriteLine("\nInt\n");
+
+            for(int i = 0; i < 7; i++)
+            {
+                ringbufferInt.Add(i);
+            }
+
+            Console.WriteLine("All The Values got added\n");
+
+            Console.WriteLine(ringbufferInt.ToString());
+
+            Console.WriteLine($"\nThe used Size: {ringbufferInt.UsedSize}\n");
+
+            ringbufferInt.Clear();
+
+            Console.WriteLine("The ringbuffer got cleared\n");
+            
+            Console.WriteLine(ringbufferInt.ToString());
+        }
+    }
+}
---
+++ RingBuffer.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RingBuffer
+{
+    public class RingBuffer<T>
+    {
+
+        // Fields
+        private int totalLength;
+        private int usedSize = 0; // How much is already in the array
+        private T[] values;
+        private int index = 0;
+
+        // Properties
+        public int UsedSize { get { return usedSize; } set { usedSize = value; } }
+        public int TotalLength { get { return totalLength;  } set { totalLength = value; } }
+
+        // Constructor
+        public RingBuffer(int totalLength)
+        {
+            if(totalLength > 0)
+            {
+                this.totalLength = totalLength;
+                values = new T[totalLength];
+            } else
+            {
+                throw new ArgumentException("You cannot add Negative Numbers");
+            }
+        }
+
+        // Methods
+        public void Add(T item)
+        {
+            if (index >= values.Length)
+            {
+                index = 0;
+
+                values[0] = item;
+
+                index++;
+            } else
+            {
+                values[index] = item;
+
+                index++;
+
+                if(usedSize < values.Length)
+                {
+                usedSize++;
+                }
+            }
+
+        }
+        public void Clear ()
+        {
+            Array.Clear(values, 0, usedSize); // Length heist wie viele (ab postion 0 -> solange bis totalValues)
+        }
+
+        public override string ToString()
+        {
+            string returnstring = string.Empty;
+            for(int i = 0; i < values.Length; i++)
+            {
+                int index = (this.index + i) % values.Length;
+
+                returnstring += values[index] + ",";
+            }
+
+            return returnstring;
+        }
+    }
+}
